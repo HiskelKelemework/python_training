40019806e837d79804f829ae582047ff331d974f
Hiskel Kelemework <hiskelkelemework6@gmail.com>
2021-08-06 16:57:37 +0300

    updated

--- 5_conditionals.py
+++ 5_conditionals.py
@@ -15,3 +15,31 @@ else:
 # if the number is even display "even"
 # if the number is evenly divisible by 5 display "multiple of five"
 # if the number is not even display "odd"
+
+# or truth table
+# -------------|
+#    |T  |  F |
+# --------|-----
+# T  | T |  T |
+# F  | T |  F |
+# -------------|
+
+
+# accept the user's age
+# accept the user's nationality
+# if the person is ethiopian and the user's age is >= 18, print "you can vote"
+# if the person is not ethipian and the user's age is >= 18 print "apply for citizenship"
+# if the person is not ethiopian and the user's age is < 18 print("no service")
+
+age = eval(input("enter your age: "))
+nationality = input("nationality: ")
+
+is_old_enough = age >= 18
+is_ethiopian = nationality == "Ethiopian"
+
+if is_ethiopian and is_old_enough:
+    print("you can vote")
+elif not is_ethiopian and is_old_enough:
+    print("apply for citizenship")
+elif not is_ethiopian and not is_old_enough:
+    print("no service")
