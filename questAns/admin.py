2ee5c0f0c02488b91e281b4716da78e9804fb694
Hiskel Kelemework <hiskelkelemework6@gmail.com>
2021-08-28 09:32:22 +0300

    added files

--- questAns/admin.py
+++ questAns/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Question, Answer
+from .models import Question, Answer, Course, Department, Teacher
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(Course)
+admin.site.register(Department)
+admin.site.register(Teacher)
--- questAns/models.py
+++ questAns/models.py
@@ -15,3 +15,19 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.description
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=60)
+    degree_earned = models.CharField(max_length=10)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+class Course(models.Model):
+    title = models.CharField(max_length=60)
+    code = models.CharField(max_length=10)
+    credit = models.IntegerField()
+    instructors = models.ManyToManyField(Teacher)
+    departments = models.ManyToManyField(Department)
